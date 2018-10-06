82dff6d5bb6c3a4c6a9e6add0a766aa1871190aa
Grey <Grey@GREY-PC>
2018-10-06 10:48:51 +0500

    First git

---
+++ BotVK/FrequencyLetters.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Text;
+
+namespace BotVK
+{
+    class FrequencyLetters
+    {
+        private int[] freqRus;
+        private int[] freqEng;
+        private int fullCount;
+        private int Length
+        {
+            get
+            {
+                return freqRus.Length + freqEng.Length;
+            }
+        }
+
+        public static FrequencyLetters operator +(FrequencyLetters fl1, FrequencyLetters fl2)
+        {
+            FrequencyLetters sum = new FrequencyLetters();
+            for (int i = 0; i < fl1.fullCount; i++)
+                sum[i] = fl1[i] + fl2[i];
+
+            sum.fullCount = fl1.fullCount + fl2.fullCount;
+
+            return sum;
+        }
+
+        public FrequencyLetters()
+        {
+            freqRus = new int[32];
+            freqEng = new int[26];
+            fullCount = 0;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                int i = index;
+                if (index > 32)
+                {
+                    i = index - 32;
+                    return freqEng[i];
+                }
+
+                return freqRus[i];
+            }
+            set
+            {
+                int i = index;
+                if (index > 32)
+                {
+                    i = index - 32;
+                    freqEng[i] = value;
+                }
+
+                freqRus[i] = value;
+            }
+        }
+        public int this[char letter]
+        {
+            get
+            {
+                int code = (int)letter;
+
+                if (code >= 97 && code <= 122)//английские буквы
+                {
+                    return freqEng[code - 97];
+                }
+
+                return freqRus[code - 1072];
+            }
+        }
+
+        public void Add(char letter)
+        {
+            int code = (int)letter;
+
+            if (code >= 1072 && code <= 1103)//русские буквы
+            {
+                freqRus[code - 1072]++;
+                fullCount++;
+            }
+
+            if (code >= 97 && code <= 122)//английские буквы
+            {
+                freqEng[code - 97]++;
+                fullCount++;
+            }
+        }
+
+        public void Add(string Text)
+        {
+            string newText = Text.Replace('ё', 'е').ToLower();
+
+            for (int i = 0; i < newText.Length; i++)
+            {
+                this.Add(newText[i]);
+            }
+        }
+
+        public string toJSON()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            bool empty = true;
+
+            sb.AppendLine("{");
+            sb.AppendLine("  \"Language\": \"Русский\",");
+            sb.Append("  \"Letters\": {");
+            for (int i = 0; i < freqRus.Length; i++)
+            {
+                if (freqRus[i] > 0)
+                {
+                    string c = ((char)(i + 1072)).ToString();
+                    if (i == 5)
+                        c = "е/ё";
+                    sb.Append(string.Format("\"{0}\": \"{1}\", ", c, Math.Round(((double)freqRus[i] / fullCount), 3)));
+                    empty = false;
+                }
+            }
+            if (!empty)
+                sb.Remove(sb.Length - 2, 2);
+            sb.AppendLine(" },");
+
+            sb.AppendLine("  \"Language\": \"English\",");
+            sb.Append("  \"Letters\": {");
+            for (int i = 0; i < freqEng.Length; i++)
+            {
+                if (freqEng[i] > 0)
+                {
+                    sb.Append(string.Format("\"{0}\": \"{1}\", ", (char)(i + 97), Math.Round(((double)freqEng[i] / fullCount), 3)));
+                    empty = false;
+                }
+            }
+            if (!empty)
+                sb.Remove(sb.Length - 2, 2);
+
+            sb.AppendLine(" }");
+            sb.Append("}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ BotVK/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+
+namespace BotVK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VkApi api = new VkApi();
+            Console.WriteLine("Привет, пользователь! Нам необходимо авторизоваться в ВК, поэтому введи свой email или телефон");
+            string line = Console.ReadLine();
+            bool auth = false;
+            do
+            {
+                string login = line;
+
+                Console.WriteLine("Хорошо! А теперь введи свой пароль. Я никому его не скажу");
+                string password = Console.ReadLine();
+
+                Console.Clear();
+
+                try//не предусмотрена обратная связь вслучае неудачи авторизации
+                {
+                    api.Authorize(new ApiAuthParams
+                    {
+                        ApplicationId = 6707209,
+                        Login = login,
+                        Password = password,
+                        Settings = Settings.All
+                    });
+
+                    auth = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Упс! Что-то пошло не так и авторизоваться не получилось.");
+                    Console.WriteLine("Давай попробуем еще раз! Введи свой email или телефон.");
+                    Console.WriteLine("Если хочешь завершить работу программы, введи пустую строку");
+
+                    line = Console.ReadLine();
+                }
+            }
+            while (line != "" && !auth);
+
+            if (!auth)
+                return;
+
+            var pi = api.Account.GetProfileInfo(); //информация о текущем профиле
+            Console.WriteLine(string.Format("Привет, {0} {1}!", pi.FirstName, pi.LastName));
+
+            string sID = "";
+            do
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Теперь давай кого-нибудь найдем. Введи ID аккаунта");
+                sID = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(sID)) //выход, если введена пустая строка
+                    continue;
+
+                long id = -1;
+
+                if (!long.TryParse(sID, out id)) //Попытка преоброзавать введнный ID  в число
+                {
+                    if (sID.StartsWith("id"))
+                        long.TryParse(sID.Substring(2), out id); //Возможно был введен в форме "id________"
+                }
+
+                List<User> u = new List<User>();
+
+                if (id > 0 && id < long.MaxValue)
+                {
+                    try
+                    {
+                        List<long> ids = new List<long>();
+                        ids.Add(id);
+                        u = api.Users.Get(ids).ToList();
+                    }
+                    catch (VkNet.Exception.InvalidUserIdException ex)
+                    {
+                        Console.WriteLine("Введен недействительный ID");
+                        continue;
+                    }
+                }
+                else
+                    u = api.Users.Search(new UserSearchParams { Query = sID }).ToList(); //поиск пользователя по введеной строке
+
+                if (u.Count == 1)
+                {
+                    string userName = string.Format("{0} {1}", u[0].FirstName, u[0].LastName);
+
+                    try
+                    {
+                        Console.WriteLine(string.Format("А я знаю, кто это! Это {0}!", userName));
+                        var posts = api.Wall.Get(new WallGetParams { OwnerId = u[0].Id, Count = 5 });
+                        
+                        FrequencyLetters fl = new FrequencyLetters();
+
+                        for (int i = 0; i < posts.WallPosts.Count; i++)
+                        {
+                            var post = posts.WallPosts[i];
+
+                            fl.Add(post.Text);
+                        }
+
+                        Console.WriteLine(fl.toJSON());
+
+                        api.Wall.Post(new WallPostParams
+                        {
+                            OwnerId = api.UserId,
+                            Message = string.Format("{0}, статистика для последних 5 постов:\r\n{1}", userName, fl.toJSON())
+                        });
+                    }
+                    catch (VkNet.Exception.UserDeletedOrBannedException ex) //нет таких признаков в результатах поиска
+                    {
+                        Console.WriteLine("Пользователь удален или заблокирован");
+                    }
+                }
+                else if (u.Count == 0)
+                    Console.WriteLine("Никого найти не удалось");
+                else
+                    Console.WriteLine("Найдено больше 1 человека");
+
+            }
+            while (sID != "");
+
+            Console.WriteLine("\r\r\rДля завершения работы нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
